f9e3215a0f3fa4c1660726458083cb86ce3ab138
andrewbramwell <andy@voucherstore.io>
2025-02-08 08:29:16 +0000

    client

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "name": "Splendid Sites Web Designs",
-    "email": "help@splendidsites.com",
+    "name": "Kru Karn Academy",
+    "email": "sukarnmorgan159@gmail.com",
     "phoneForTel": "+447961915544",
     "phoneFormatted": "07961 915544",
     "openingHours": "Mon-Sun: 10am-8pm",
-    "onlineBooking": "https://www.facebook.com/",
+    "onlineBooking": "",
     "address": {
         "lineOne": "159 Edwin Rd",
-        "lineTwo": "Second Address Line",
+        "lineTwo": "",
         "city": "Gillingham",
         "state": "Kent",
         "zip": "ME8 0AQ",
@@ -15,10 +15,9 @@ module.exports = {
         "mapLink": "https://maps.app.goo.gl/SaH722xtz5PCnam88"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/profile.php?id=100057058796223",
         "googleReview": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://krukarn.co.uk"
 };
